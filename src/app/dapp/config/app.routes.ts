16f048db6bb3b985b23e14dd94aa01021692895f
Diego Ramos Sanchez <127451062+AlexanderRamosSanchez@users.noreply.github.com>
2025-07-22 16:13:28 +0000

    [MEDIUM] : Seguridad Rutas

--- src/app/dapp/config/app.routes.ts
+++ src/app/dapp/config/app.routes.ts
@@ -1,12 +1,14 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/pages/dashboard/dashboard.component';
 import { HomeComponent } from '../components/auth/home/home.component';
-
+import { AuthGuard } from '../services/guard/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/quickcash', pathMatch: 'full' },
-  { path: 'quickcash', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: '/quickcash' }
+  { path: '', component: HomeComponent },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
 ];
